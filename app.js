e7ae64d1c20ef9162018f955e7066fb564c65c6b
mvazramos <vaz.ramosmiguel@gmail.com>
2022-10-21 19:55:34 +0100

    mouse down effect

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const container = document.getElementsByClassName("sketch-board");
-
-
-
 const gridWidth = 600; // The grid is a square
 const size=16;
+const side = Math.floor(gridWidth/size);
+
 const colors = {
     "Aqua":"#00FFFF",
     "Aquamarine":"#7FFFD4",
     "Brown":"#A52A2A",
     "Chocolate":"#D2691E"
-}
+};
+colorsArray=Object.values(colors);
+
+let isDown= false;
 
 
+document.addEventListener("mousedown",()=> isDown=true);
+document.addEventListener("mouseup",()=> isDown=false)
 
-colorsArray=Object.values(colors)
 
-const side = Math.floor(gridWidth/size);
 
 for (let i = 0; i<size; i++) {
     /* This creates a total of N divs which will take another N divs inside
@@ -42,15 +44,22 @@ const squares =  document.getElementsByClassName("square");
 
 function changeColor(e){
     c = colorsArray[Math.floor(Math.random() * colorsArray.length)]
-    e.target.style.backgroundColor=c;
+    
+    colorMode="black";
+    if(e.type==="mousemove" && !isDown ) return 
+        //e.target.style.backgroundColor=c;
+    else if(colorMode==='black'){
+        e.target.style.backgroundColor=c;
+    }
 
+    console.log(e)
     
 }
 
 
 Array.from(squares).forEach( (square) => {
-    square.addEventListener("mouseenter", changeColor)
-    
+    square.addEventListener("mousedown", changeColor);
+    square.addEventListener("mousemove", changeColor);
 });
 
 
