520c78d0b03f8b455ce2540fa5af08df1d301bdb
miguelvazramos <vaz.ramosmiguel@gmail.com>
2022-10-24 19:48:19 +0100

    first iteration with functionality

--- app.js
+++ app.js
@@ -1,66 +1,97 @@
+let isDragging= false;
+document.addEventListener("mousedown",()=> isDragging=true);
+document.addEventListener("mouseup",()=> isDragging=false);
 const container = document.getElementsByClassName("sketch-board");
-const gridWidth = 600; // The grid is a square
-const size=16;
-const side = Math.floor(gridWidth/size);
+const gridWidth = 700; // The grid is a square
 
-const colors = {
-    "Aqua":"#00FFFF",
-    "Aquamarine":"#7FFFD4",
-    "Brown":"#A52A2A",
-    "Chocolate":"#D2691E"
-};
-colorsArray=Object.values(colors);
+let colorMode="black";
 
-let isDown= false;
+const reset= document.getElementsByTagName("button");
+reset[0].addEventListener("click",cleanBoard);
+
+const colorsquares = document.getElementsByClassName("color-square");
+const gridSize= document.getElementById("gridSetter");
 
 
-document.addEventListener("mousedown",()=> isDown=true);
-document.addEventListener("mouseup",()=> isDown=false)
+gridSize.addEventListener("input", (e)=>{
+    const p = document.getElementById("gridSize");
+    const container = document.getElementsByClassName("sketch-board");
+    container[0].innerHTML="";
+    createBoard(e.target.value);
+    p.innerHTML=e.target.value;
+})
 
 
 
-for (let i = 0; i<size; i++) {
-    /* This creates a total of N divs which will take another N divs inside
-       getting in this way a N x N board.
-    */
-    const div = document.createElement("div");
-    div.setAttribute("class","row");
-    for (let j=0; j<size; j++){
-        const divChild = document.createElement("div");
-        divChild.style.backgroundColor="white"
-        divChild.style.width=`${side}px`;
-        divChild.style.height=`${side}px`;
-        divChild.style.border="1px solid black";
-        divChild.setAttribute("class","square")
-        div.appendChild(divChild);
-    }
-    container[0].appendChild(div);    
+
+
+Array.from(colorsquares).forEach((e)=>{
+    e.addEventListener("click",(e)=>{
+        colorMode=e.target.id;
+    })
+});
+
+
+function cleanBoard(){
+    const squares =  document.getElementsByClassName("square");
+    Array.from(squares).forEach(square=>{
+        square.style.backgroundColor="white";
+    });
 }
 
+function createBoard(enterSize){
+    /* 
+        This creates a total of N divs which will take another N divs inside
+        getting in this way a N x N board.
+    */
 
-const squares =  document.getElementsByClassName("square");
-
+    const size=enterSize;
+    const side = gridWidth/size;
+    for (let i = 0; i<size; i++) {
+        
+        const div = document.createElement("div");
+        div.setAttribute("class","row");
+        for (let j=0; j<size; j++){
+            const divChild = document.createElement("div");
+            divChild.style.backgroundColor="white"
+            divChild.style.width=`${side}px`;
+            divChild.style.height=`${side}px`;
+            //divChild.style.border="0.01px solid black";
+            divChild.style.outlineStyle="solid";
+            divChild.style.outlineColor="black";
+            divChild.style.outlineWidth="1px";
+            divChild.setAttribute("class","square")
+            div.appendChild(divChild);
+            divChild.addEventListener("mousedown", changeColor);
+            divChild.addEventListener("mousemove", changeColor);
+        }
+        container[0].appendChild(div);    
+    }
+}
 
 
 function changeColor(e){
-    c = colorsArray[Math.floor(Math.random() * colorsArray.length)]
-    
-    colorMode="black";
-    if(e.type==="mousemove" && !isDown ) return 
-        //e.target.style.backgroundColor=c;
-    else if(colorMode==='black'){
-        e.target.style.backgroundColor=c;
-    }
 
-    console.log(e)
+    let r = Math.floor(Math.random()*255);
+    let g =Math.floor(Math.random()*255);
+    let b =Math.floor(Math.random()*255); 
     
+    if(e.type==="mousemove" && !isDragging ) return 
+        //e.target.style.backgroundColor=c;
+    else if(colorMode==="rgb-color"){
+        e.target.style.backgroundColor=`rgb(${r},${g},${b})`
+    }
+    else{
+        e.target.style.backgroundColor=colorMode;
+    } 
 }
 
 
-Array.from(squares).forEach( (square) => {
-    square.addEventListener("mousedown", changeColor);
-    square.addEventListener("mousemove", changeColor);
-});
+
+window.onload= ()=>{
+    createBoard(16);
+};
+
 
 
 
